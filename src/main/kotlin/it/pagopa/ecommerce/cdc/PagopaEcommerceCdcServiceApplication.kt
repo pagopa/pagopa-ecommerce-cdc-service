11b8f9dbc2cca41533fc45e81fd69235dee7584d
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-07-10 17:23:56 +0200

    feat(cdc): [CHK-4318] add base cdc config classes

--- src/main/kotlin/it/pagopa/ecommerce/cdc/PagopaEcommerceCdcServiceApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/PagopaEcommerceCdcServiceApplication.kt
@@ -1,9 +1,19 @@
 package it.pagopa.ecommerce.cdc
 
+import it.pagopa.ecommerce.cdc.config.properties.ChangeStreamOptionsConfig
+import it.pagopa.ecommerce.cdc.config.properties.RetrySendPolicyConfig
+import it.pagopa.ecommerce.cdc.config.properties.RetryStreamPolicyConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication class PagopaEcommerceCdcServiceApplication
+@SpringBootApplication
+@EnableConfigurationProperties(
+    ChangeStreamOptionsConfig::class,
+    RetrySendPolicyConfig::class,
+    RetryStreamPolicyConfig::class,
+)
+class PagopaEcommerceCdcServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<PagopaEcommerceCdcServiceApplication>(*args)
