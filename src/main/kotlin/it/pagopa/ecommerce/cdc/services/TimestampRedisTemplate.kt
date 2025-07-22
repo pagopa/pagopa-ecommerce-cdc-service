ee9d808943b2017d1cf4a586ab4436f357a314d5
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-07-22 16:27:11 +0200

    fix(redis-resume): [CHK-4346] replace method calls with indexed accessors

--- src/main/kotlin/it/pagopa/ecommerce/cdc/services/TimestampRedisTemplate.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/services/TimestampRedisTemplate.kt
@@ -5,17 +5,25 @@ import java.time.Instant
 import java.util.*
 import kotlin.text.format
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ValueOperations
 import org.springframework.stereotype.Component
+
+// extension function for indexed accessor syntax with TTL
+// this is to avoid sonar warning on rule kotlin:S6518
+// https://next.sonarqube.com/sonarqube/coding_rules?open=kotlin%3AS6518&rule_key=kotlin%3AS6518
+operator fun ValueOperations<String, Instant>.set(key: String, ttl: Duration, value: Instant) {
+    set(key, value, ttl)
+}
 
 @Component
 class TimestampRedisTemplate(private val redisTemplate: RedisTemplate<String, Instant>) {
     fun save(keyspace: String, cdcTarget: String, instant: Instant, ttl: Duration) {
-        redisTemplate.opsForValue().set(compoundKeyWithKeyspace(keyspace, cdcTarget), instant, ttl)
+        redisTemplate.opsForValue()[compoundKeyWithKeyspace(keyspace, cdcTarget), ttl] = instant
     }
 
     fun findByKeyspaceAndTarget(keyspace: String, cdcTarget: String): Optional<Instant> {
         return Optional.ofNullable(
-            redisTemplate.opsForValue().get(compoundKeyWithKeyspace(keyspace, cdcTarget))
+            redisTemplate.opsForValue()[compoundKeyWithKeyspace(keyspace, cdcTarget)]
         )
     }
 
