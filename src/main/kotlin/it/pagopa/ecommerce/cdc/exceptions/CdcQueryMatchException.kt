2511577e627063fc85851a523155f385bff7867c
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-08-07 14:36:26 +0200

    chore(doc): [CHK-4377] update exceptions kdocs

--- src/main/kotlin/it/pagopa/ecommerce/cdc/exceptions/CdcQueryMatchException.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/exceptions/CdcQueryMatchException.kt
@@ -1,4 +1,10 @@
 package it.pagopa.ecommerce.cdc.exceptions
 
+/**
+ * Exception thrown when a CDC database query operation fails to match expected conditions.
+ *
+ * @param message Details about the query matching failure
+ * @param throwable The underlying cause, if any
+ */
 class CdcQueryMatchException(message: String, throwable: Throwable? = null) :
     RuntimeException(message, throwable)
