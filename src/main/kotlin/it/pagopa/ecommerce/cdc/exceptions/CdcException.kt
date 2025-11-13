26fce22fbb24591a135ecc752b3622a762aa4669
pietro.tota <pietro.tota@pagopa.it>
2025-11-13 11:56:51 +0100

    feat: add junit tests

--- src/main/kotlin/it/pagopa/ecommerce/cdc/exceptions/CdcException.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/exceptions/CdcException.kt
@@ -1,7 +1,7 @@
 package it.pagopa.ecommerce.cdc.exceptions
 
-abstract class CdcException(
+sealed class CdcException(
     open val retriableError: Boolean,
     message: String,
     throwable: Throwable? = null,
-) : Exception("$message - retriable error: [$retriableError]", throwable) {}
+) : Exception("$message - retriable error: [$retriableError]", throwable)
