cba1b08cb67fe2feb79b5e68a7f8d0b80f6e24ce
pietro.tota <pietro.tota@pagopa.it>
2025-12-03 14:48:12 +0100

    chore: test disabling lock

--- src/main/kotlin/it/pagopa/ecommerce/cdc/services/CdcLockService.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/services/CdcLockService.kt
@@ -1,9 +1,6 @@
 package it.pagopa.ecommerce.cdc.services
 
-import it.pagopa.ecommerce.cdc.config.properties.RedisJobLockPolicyConfig
 import it.pagopa.ecommerce.cdc.exceptions.CdcEventProcessingLockNotAcquiredException
-import java.util.concurrent.TimeUnit
-import org.redisson.api.RedissonReactiveClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -18,8 +15,6 @@ import reactor.core.publisher.Mono
  */
 @Service
 class CdcLockService(
-    private val redissonClient: RedissonReactiveClient,
-    private val redisJobLockPolicyConfig: RedisJobLockPolicyConfig,
 ) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
@@ -38,15 +33,6 @@ class CdcLockService(
      */
     fun acquireEventLock(eventId: String): Mono<Boolean> {
         logger.debug("Trying to acquire lock for event: {}", eventId)
-        return Mono.defer {
-                redissonClient
-                    .getLock(redisJobLockPolicyConfig.getLockNameByEventId(eventId))
-                    .tryLock(
-                        redisJobLockPolicyConfig.waitTimeInMs,
-                        redisJobLockPolicyConfig.ttlInMs,
-                        TimeUnit.MILLISECONDS,
-                    )
-            }
-            .onErrorMap { CdcEventProcessingLockNotAcquiredException(eventId, it) }
+        return Mono.just(true)
     }
 }
