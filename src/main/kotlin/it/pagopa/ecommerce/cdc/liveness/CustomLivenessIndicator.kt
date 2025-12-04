2a85e1dd3bd9486977837b4458e677adbc744fe7
pietro.tota <pietro.tota@pagopa.it>
2025-12-04 15:48:42 +0100

    feat: add custom liveness probe

--- src/main/kotlin/it/pagopa/ecommerce/cdc/liveness/CustomLivenessIndicator.kt
+++ src/main/kotlin/it/pagopa/ecommerce/cdc/liveness/CustomLivenessIndicator.kt
@@ -1,5 +1,8 @@
 package it.pagopa.ecommerce.cdc.liveness
 
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.atomic.AtomicBoolean
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.availability.LivenessStateHealthIndicator
@@ -7,46 +10,49 @@ import org.springframework.boot.availability.ApplicationAvailability
 import org.springframework.boot.availability.AvailabilityState
 import org.springframework.boot.availability.LivenessState
 import org.springframework.stereotype.Component
-import java.time.Duration
-import java.time.Instant
-import java.util.concurrent.atomic.AtomicBoolean
 
 @Component
 class CustomLivenessIndicator(
     availability: ApplicationAvailability,
-    @Value($$"${customLivenessIndicator.cdc.inactivityTimeoutMillis}") val inactivityTimeoutMillis: Long
+    @Value($$"${customLivenessIndicator.cdc.inactivityTimeoutSeconds}")
+    val inactivityTimeoutSeconds: Long,
 ) : LivenessStateHealthIndicator(availability) {
 
     companion object {
         val cdcStreamUpAndRunning = AtomicBoolean(false)
 
         /*
-            no need for an atomic reference here, concurrency updating this field can be
-            ignored.
-            The target here is to check that CDC is processing events, no need for synchronized access to this variable
-         */
+           no need for an atomic reference here, concurrency updating this field can be
+           ignored.
+           The target here is to check that CDC is processing events, no need for synchronized access to this variable
+        */
         var lastDequeuedEventAt: Instant = Instant.MIN
     }
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    override fun getState(applicationAvailability: ApplicationAvailability): AvailabilityState {
+    public override fun getState(
+        applicationAvailability: ApplicationAvailability
+    ): AvailabilityState {
         val state = super.getState(applicationAvailability) as LivenessState
         if (state != LivenessState.CORRECT) {
             return state
         }
 
-        //app state is correct, checking CDC parameters
+        // app state is correct, checking CDC parameters
         if (!cdcStreamUpAndRunning.get()) {
             logger.error("App is not alive, detected CDC stream to not be alive")
             return LivenessState.BROKEN
         }
-        val inactivityTimeout = Duration.ofMillis(inactivityTimeoutMillis)
-        if (Duration.between(lastDequeuedEventAt, Instant.now()).abs() > Duration.ofMillis(inactivityTimeoutMillis)) {
+        val inactivityTimeout = Duration.ofMillis(inactivityTimeoutSeconds)
+        if (
+            Duration.between(lastDequeuedEventAt, Instant.now()).abs() >
+                Duration.ofSeconds(inactivityTimeoutSeconds)
+        ) {
             logger.error(
                 "CDC inactivity detected. Last dequeued event at: [{}], inactivity timeout: [{}]",
                 lastDequeuedEventAt,
-                inactivityTimeout
+                inactivityTimeout,
             )
             return LivenessState.BROKEN
         }
---
+++ src/test/kotlin/it/pagopa/ecommerce/cdc/liveness/CustomLivenessIndicatorTest.kt
@@ -0,0 +1,67 @@
+package it.pagopa.ecommerce.cdc.liveness
+
+import java.time.Duration
+import java.time.Instant
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import org.springframework.boot.availability.ApplicationAvailability
+import org.springframework.boot.availability.LivenessState
+
+class CustomLivenessIndicatorTest {
+
+    private val applicationAvailability: ApplicationAvailability = mock()
+
+    private val inactivityTimeoutSeconds = 10L
+
+    private val customLivenessIndicator =
+        CustomLivenessIndicator(applicationAvailability, inactivityTimeoutSeconds)
+
+    @Test
+    fun `Should propagate error status from superclass`() {
+        // pre-condition
+        given(applicationAvailability.livenessState).willReturn(LivenessState.BROKEN)
+        // test
+        val state = customLivenessIndicator.getState(applicationAvailability)
+        // assertions
+        assertEquals(LivenessState.BROKEN, state)
+    }
+
+    @Test
+    fun `Should return broken status for CDC not up and running detected`() {
+        // pre-condition
+        given(applicationAvailability.livenessState).willReturn(LivenessState.CORRECT)
+        CustomLivenessIndicator.cdcStreamUpAndRunning.set(false)
+        // test
+        val state = customLivenessIndicator.getState(applicationAvailability)
+        // assertions
+        assertEquals(LivenessState.BROKEN, state)
+    }
+
+    @Test
+    fun `Should return broken status for inactive CDC detected (no event processed inside threshold)`() {
+        // pre-condition
+        given(applicationAvailability.livenessState).willReturn(LivenessState.CORRECT)
+        CustomLivenessIndicator.cdcStreamUpAndRunning.set(true)
+        CustomLivenessIndicator.lastDequeuedEventAt =
+            Instant.now() - Duration.ofSeconds(inactivityTimeoutSeconds + 1)
+        // test
+        val state = customLivenessIndicator.getState(applicationAvailability)
+        // assertions
+        assertEquals(LivenessState.BROKEN, state)
+    }
+
+    @Test
+    fun `Should return correct status for for healthy app`() {
+        // pre-condition
+        given(applicationAvailability.livenessState).willReturn(LivenessState.CORRECT)
+        CustomLivenessIndicator.cdcStreamUpAndRunning.set(true)
+        CustomLivenessIndicator.lastDequeuedEventAt =
+            Instant.now() - Duration.ofSeconds(inactivityTimeoutSeconds - 1)
+        // test
+        val state = customLivenessIndicator.getState(applicationAvailability)
+        // assertions
+        assertEquals(LivenessState.CORRECT, state)
+    }
+}
