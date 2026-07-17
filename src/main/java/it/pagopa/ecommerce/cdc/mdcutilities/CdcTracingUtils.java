41c09f6ead78d8c0ad456e142268d7f3e9defcc2
danilo.ciano <danilo.ciano@pagopa.it>
2026-07-17 14:20:17 +0200

    chore: wip

--- src/main/java/it/pagopa/ecommerce/cdc/mdcutilities/CdcTracingUtils.java
+++ src/main/java/it/pagopa/ecommerce/cdc/mdcutilities/CdcTracingUtils.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import reactor.util.context.Context;
 
